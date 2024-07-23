f115546f1ac0f35302e97329b7c65456b5c8fc75
annasshah <30356728+annasshah@users.noreply.github.com>
2024-07-23 19:39:11 +0500

    conditional statement class

---
+++ conditional_statement/main.dart
@@ -0,0 +1,17 @@
+void main() {
+  int percentage = 70;
+  if (percentage < 40) {
+    print("fail");
+  } else {
+    if (percentage >= 50) {
+      print("Pass with C Grade");
+    } else if (percentage >= 40) {
+      print("Pass");
+    } else if (percentage >= 30) {
+      print("Pass");
+    } else {
+      print("fail");
+    }
+  }
+  print("object");
+}
