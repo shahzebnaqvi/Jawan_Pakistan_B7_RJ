630da39683125cb27b19bf0b2a4a12c1855257c9
hamzakamelen <hamzakamelen12345@gmail.com>
2024-08-06 20:02:37 +0500

    map class code

--- map/main.dart
+++ map/main.dart
@@ -1,34 +1,66 @@
-import 'dart:math';
-
 void main() {
-  List stdName = [
-    {
-      "name": {"first": 2323, "last": "23wesdd"},
-      "age": 83,
-      "class": "xyz",
-    },
-    {
-      "name": "abc",
-      "age": 70,
-      "class": "xyz",
-      "name": "iisdsd",
-    }
-  ];
-  print(stdName[0]['name']['first']);
+  // Map std2 = {
+  //   "name": {"First": "Shahzeb", "last": "Naqvi"},
+  //   "class": 12,
+  //   "Age": 10,
+  // };
+  // List<Map> studentName = [std1, std2];
+
+  // print(studentName[0]["name"]["First"]);
+  // studentName.clear();
   Map std1 = {
-    "name": "abc",
-    "age": 70,
-    "class": "xyz",
-    "name": "iisdsd",
+    "name": "Abdullah",
+    "class": 12,
+    "Age": 10,
   };
-  Map std2 = {"name": "xyz", "age": 70, "class": "xyz"};
-  print(std1["name"]);
-  print(std1["age"]);
-  print(std2["name"]);
-  print(std2["age"]);
-  // std2.clear();
-  print(std2.keys.toList());
-  print(std1.length);
-  print(std1.isEmpty);
-  print(std1.isNotEmpty);
+  Map std1bio = {
+    "address": "asdfghjk",
+    "phone": 32333,
+    "gender": "MAle",
+  };
+  Map std1fee = {
+    "Month": "JAN",
+    "fees": 3000,
+    "lab_charges": 200,
+    "gender": "Female",
+  };
+  // std1.clear();
+  // print(std1.keys.toList());
+  // print(std1.values);
+  // print(std1.isEmpty);
+  // print(std1.isNotEmpty);
+  // std1["name"] = "shahzeb";
+  // std1["LastName"] = "Naqvi";
+  // std1.putIfAbsent("name", () => "Shahzeb");
+  // std1.putIfAbsent("Lastname", () => "Naqvi");
+  // std1.addAll({"Address": "sdsd", "phne": "3222"});
+  // std1.remove("Age");
+  // std1.removeWhere((key, value) => value == "Abdullah");
+  // std1
+  //   ..addAll(std1bio)
+  //   ..addAll(std1fee)
+  //   ..addAll(std1fee)
+  //   ..addAll(std1fee)
+  //   ..addAll(std1fee)
+  //   ..addAll(std1fee)
+  //   ..addAll(std1bio);
+  // var abc = {
+  //   ...std1bio,
+  //   ...std1fee,
+  //   ...std1bio,
+  // };
+  // print(abc);
+  List students = ["std1", "abc", "std3", "std4", "std5"];
+  List studentsName = ["Shahzeb", 33, "Abdullah", "Mustafa", "Maaz"];
+  Map StdData = Map.fromIterables(students, studentsName);
+  bool ishaveKey = StdData.containsKey("Amount");
+  bool ishaveValue = StdData.containsValue("Ali");
+  String testdata = "shahzeb";
+  print(testdata.startsWith("shaz"));
+  print(ishaveKey);
+  print(ishaveValue);
+  if (ishaveKey) {
+    StdData["AMount"] = StdData["AMount"] + 300;
+  }
+  print(StdData);
 }
